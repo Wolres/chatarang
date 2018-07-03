7db343e11cddd66d0c345f7f183b2621ba38528d
Harrison Yelton <hayelton@iu.edu>
2018-07-03 11:06:00 -0400

    Add messages to Chat's slate

--- src/Chat.js
+++ src/Chat.js
@@ -5,11 +5,21 @@ import MessageList from './MessageList'
 import MessageForm from './MessageForm'
 
 class Chat extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            messages: [
+                { id: 1, userName: 'dstrus', body: 'A giant chatter box, am I!' },
+                { id: 2, userName: 'dpalazzo', body: 'Hello there!' },
+            ],
+        }
+    }
     render() {
         return (
             <div className="Chat">
                 <ChatHeader />
-                <MessageList />
+                <MessageList messages={this.state.messages} />
                 <MessageForm />
             </div>
         )
--- src/MessageList.js
+++ src/MessageList.js
@@ -2,16 +2,11 @@ import React from 'react'
 
 import Message from './Message'
 
-const MessageList = () => {
-    const messages = [
-        { id: 1, userName: 'dstrus', body: 'A giant chatter box, am I!' },
-        { id: 2, userName: 'dpalazzo', body: 'Hello there!' },
-    ]
-
+const MessageList = (props) => {
     return (
         <div className="MessageList">
             { 
-            messages.map(msg => (
+            props.messages.map(msg => (
                 <Message message={msg} key={msg.id} />
             )) 
             }
