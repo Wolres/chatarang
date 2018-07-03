20c6bdde1080fbd1d6c73f7de4686e478b35eb8a
Harrison Yelton <hayelton@iu.edu>
2018-07-03 15:26:02 -0400

    Add styling to Message

--- src/Message.js
+++ src/Message.js
@@ -2,10 +2,21 @@ import React from 'react'
 
 const Message = (props) => {
     return (
-        <div className="Message">
+        <div className="Message" style={styles.message}>
             {props.message.user.displayName}: {props.message.body}
         </div>
     )
+}
+
+const styles = {
+    message: {
+        display: 'flex',
+        marginTop: '1rem',
+        padding: '0 1rem',
+        // was in another style tag, not sure what these do
+        flex: 1,
+        paddingLeft: '0.5rem',
+    }
 }
 
 export default Message
