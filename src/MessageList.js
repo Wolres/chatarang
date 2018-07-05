6c687b641495eaae4987320e7b94e4278770cea6
Harrison Yelton <hayelton@iu.edu>
2018-07-05 06:56:21 -0400

    Added a RoomAnnouncement component and style rules

--- src/MessageList.js
+++ src/MessageList.js
@@ -1,17 +1,27 @@
 import React from 'react'
 
 import Message from './Message'
+import RoomAnnouncement from './RoomAnnouncement'
 
 const MessageList = (props) => {
     return (
-        <div className="MessageList">
-            { 
-            props.messages.map(msg => (
-                <Message message={msg} key={msg.id} />
-            )) 
+        <div className="MessageList" style={styles.messageList}>
+            <RoomAnnouncement />
+            {props.messages.map(msg => 
+                    (<Message message={msg} key={msg.id} />)
+                )
             }
         </div>
     )
+}
+
+const styles = {
+    messageList: {
+        backgroundColor: 'white',
+        flex: 1,
+        paddingBottom: '1rem',
+        overflowY: 'scroll',
+    }
 }
 
 export default MessageList
