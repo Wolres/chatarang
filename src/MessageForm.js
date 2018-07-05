1d9ea22b1b1b53283930392915eca2cb8be26ad0
Harrison Yelton <hayelton@iu.edu>
2018-07-05 06:57:36 -0400

    Add style rules to MessageForm

--- src/MessageForm.js
+++ src/MessageForm.js
@@ -20,6 +20,7 @@ class MessageForm extends Component {
             <form 
                 className="MessageForm"
                 onSubmit={this.handleSubmit}
+                style={styles.messageForm}
             >
                 <input
                     type="text"
@@ -29,12 +30,53 @@ class MessageForm extends Component {
                     onChange={this.handleChange}
                     autoFocus
                     required
+                    style={styles.input}
                 />
-                <button type="submit">
-                    Send
+                <button type="submit" style={styles.button}>
+                    <i className="fas fa-bullhorn" title="send"></i>
                 </button>
             </form>
         )
+    }
+}
+
+const styles = {
+    messageForm: {
+        backgroundColor: 'white',
+        height: '3rem',
+        display: 'flex',
+        alignItems: 'stretch',
+        border: '2px solid #999',
+        borderRadius: '0.5rem',
+        margin: '0.25rem',
+        padding: 0
+    },
+
+    chatIcon: {
+            display: 'flex',
+            borderRadius: '0.5rem',
+            alignItems: 'center',
+            backgroundColor: 'white',
+            color: '#ccc',
+            padding: '0 0.5rem',
+            fontSize: '1.2rem',
+        },
+
+    input: {
+        flex: 1,
+        fontSize: '1.2rem',
+        border: 0,
+    },
+    
+    button: {
+        fontSize: '1.5rem',
+        backgroundColor: '#1A8FE3',
+        color: 'white',
+        paddingLeft: '1rem',
+        paddingRight: '1rem',
+        borderTopRightRadius: '0.5rem',
+        borderBottomRightRadius: '0.5rem',
+        border: '1px solid white',
     }
 }
 
