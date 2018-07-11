7f1e6bbe0415bd4b65bda5c05aff8284bc2fe6b2
Harrison Yelton <hayelton@iu.edu>
2018-07-11 09:22:12 -0400

    Display a timestamp for each message

--- src/Metadata.js
+++ src/Metadata.js
@@ -10,7 +10,7 @@ const Metadata = ({ message }) => {
         {message.user.displayName}
       </div>
       <div style={styles.time}>
-        1:10 PM
+        {moment(message.createdAt).format('D MMM @ h:mm A')}
       </div>
     </div>
   )
